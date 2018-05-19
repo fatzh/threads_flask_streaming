31ae9b9c2c80cc01ffc0cc9271bf509b4c80e518
fatz <fabrice.tz@51stfloor.com>
2018-05-19 20:11:18 +0200

    initial version

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, Response
+from flask_restful import Resource, Api
+from camera import Camera
+
+application = Flask(__name__)
+api = Api(application)
+
+cam = Camera(application)
+
+
+def gen_frames():
+    '''
+    Generator function, creates a comet of continous images
+    '''
+    try:
+        while True:
+            frame = cam.get_frame()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    except Exception as e:
+        print(e)
+
+
+class Stream(Resource):
+    '''
+    Streaming endpoint, starts a new thread to provide continous images
+    '''
+    def get(self):
+        cam.stream_start()
+        return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+class StreamStop(Resource):
+    '''
+    Stops streaming, free the camera resource
+    '''
+    def get(self):
+        cam.stream_stop()
+        return 200
+
+
+class Capture(Resource):
+    '''
+    Capture an image / adds a new page to a document
+    '''
+
+    def get(self):
+        cam.capture()
+        return {
+            'capture': 'ok',
+            'streaming': cam.is_streaming(),
+        }
+
+
+class Hello(Resource):
+    def get(self):
+        return {
+            'streaming': cam.is_streaming(),
+        }
+
+
+# actually bind rounting
+api.add_resource(Stream, '/stream')
+api.add_resource(StreamStop, '/stream/stop')
+api.add_resource(Capture, '/capture')
+api.add_resource(Hello, '/hello')
+
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', threaded=True, debug=True)
---
+++ camera.py
@@ -0,0 +1,70 @@
+from PIL import Image
+import io
+import time
+from random import randint
+import threading
+
+STREAM_TIMEOUT = 2
+
+
+class Camera():
+
+    application = None
+    streaming = False  # class variable, used to check the current status
+
+    frame = None  # current frame from streaming
+    last_frame_request = None  # timestamp last requested frame from streaming, to free resources in case of inactivity
+
+    thread = None  # that's the streaming thread
+
+    def __init__(self, app):
+        self.application = app
+
+    def stream_start(self):
+        Camera.last_frame_request = time.time()
+        Camera.streaming = True
+        if Camera.thread is None:
+            Camera.thread = threading.Thread(target=Camera._stream)
+            Camera.thread.start()
+        while Camera.frame is None:
+            time.sleep(0.05)
+        return True
+
+    def stream_stop(self):
+        if Camera.streaming or Camera.thread:
+            Camera.streaming = False
+            try:
+                Camera.thread.join()
+            except AttributeError:
+                pass
+            Camera.thread = None
+        return 200
+
+    @classmethod
+    def _stream(cls):
+        while(cls.streaming and time.time() - cls.last_frame_request < STREAM_TIMEOUT):
+            time.sleep(0.5)
+            g = randint(0, 255)
+            r = randint(0, 255)
+            b = randint(0, 255)
+            img = Image.new('RGB', (800, 800), (r, g, b))
+            out = io.BytesIO()
+            img.save(out, format='JPEG')
+            cls.frame = out.getvalue()
+
+        # we arrive here if streaming is set to Flase (by stream_stop for
+        # example) or if no frame has been requested in the last X seconds
+        cls.streaming = False
+        cls.thread = None
+
+    def get_frame(self):
+        time.sleep(0.5)
+        Camera.last_frame_request = time.time()
+        return self.frame
+
+    def capture(self):
+        self.stream_stop()
+        return True
+
+    def is_streaming(self):
+        return Camera.streaming
